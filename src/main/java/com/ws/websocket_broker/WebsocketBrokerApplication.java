e93bebb75213247fff570108456f5cb14e83c508
rxmann <k.romann413@gmail.com>
2026-02-21 22:22:33 +0545

    feat: ws init chat / notification demo project

---
+++ src/main/java/com/ws/websocket_broker/WebsocketBrokerApplication.java
@@ -0,0 +1,13 @@
+package com.ws.websocket_broker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsocketBrokerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsocketBrokerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ws/websocket_broker/WebsocketBrokerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ws.websocket_broker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsocketBrokerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
